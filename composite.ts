d8898fa3f57f8307ba1dca53fea2f165de58b1e1
ANDRES PORRAS VERGARAY <andresporras@MacBook-Pro-de-ANDRES-2.local>
2020-01-23 17:22:25 -0500

    Se desarrollo el patron composite

--- composite.ts
+++ composite.ts
@@ -1,68 +1,177 @@
-abstract class ComponentePC
+//solamente va a almacenar a las ramas
+abstract class RamaPC
 {
-    abstract calcularPrecio() : number 
-}
-
-class Computador extends ComponentePC{
-    calcularPrecio(): number {
-        
+    hijos : ComponentePC[] | null
+    addComponente(comp : ComponentePC)
+    {
+        //lazy initialization, se instancia cuando se requiere
+        if (this.hijos == null)
+        {
+            this.hijos = []
+        }
+        this.hijos.push(comp)
     }
 }
 
-class Monitor extends ComponentePC{
-    calcularPrecio(): number {
-        
+interface ComponentePC
+{
+    //hijos : ComponentePC[] | null = null
+    calcularPrecio() : number //antes era abstract
+}
+
+class Computador extends RamaPC implements ComponentePC
+{
+    /*addComponente(comp : ComponentePC)
+    {
+        //lazy initialization, se instancia cuando se requiere
+        if (this.hijos == null)
+        {
+            this.hijos = []
+        }
+        this.hijos.push(comp)
+    }*/
+
+    calcularPrecio() : number 
+    {
+        let sumaPrecios = 0
+        if (this.hijos != null)
+        {
+            
+            for (let i=0; i < this.hijos.length ; i++)
+            {
+                let precioHijo = this.hijos[i].calcularPrecio()
+                sumaPrecios += precioHijo
+                
+                //sumaPrecios += this.hijos[i].calcularPrecio()
+            }
+            return sumaPrecios
+        }
     }
 }
 
-class Mouse extends ComponentePC{
-    calcularPrecio(): number {
-        
+class Monitor implements ComponentePC //antes era extends porque ComponentePC era abstract class
+{
+    calcularPrecio(): number 
+    {
+        return 200
     }
 }
 
-class Case extends ComponentePC{
-    calcularPrecio(): number {
-        
+class Mouse implements ComponentePC
+{
+    calcularPrecio(): number 
+    {
+        return 20
     }
 }
 
-class Teclado extends ComponentePC{
-    calcularPrecio(): number {
-        
+//esta es una rama de un arbol ya que puede tener hijos, por ello tiene ese calcularPrecio() implementado
+class Case extends RamaPC implements ComponentePC
+{
+    /*addComponente(comp : ComponentePC)
+    {
+        //lazy initialization, se instancia cuando se requiere
+        if (this.hijos == null)
+        {
+            this.hijos = []
+        }
+        this.hijos.push(comp)
+    }*/
+
+    calcularPrecio(): number 
+    {
+        let sumaPrecios = 0
+        if (this.hijos != null)
+        {
+            
+            for (let i=0; i < this.hijos.length ; i++)
+            {
+                let precioHijo = this.hijos[i].calcularPrecio()
+                sumaPrecios += precioHijo
+                
+                //sumaPrecios += this.hijos[i].calcularPrecio()
+            }
+            return sumaPrecios
+        }
     }
 }
 
-class Memoria extends ComponentePC{
-    calcularPrecio(): number {
-        
+//este es una hoja, ya que no puede tener hijo, depende de las ramas
+class Teclado implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 20
     }
 }
 
-class Placa extends ComponentePC{
-    calcularPrecio(): number {
-        
+class Memoria implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 90
     }
 }
 
-class TarjetaGrafica extends ComponentePC{
-    calcularPrecio(): number {
-        
+class Placa implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 100
     }
 }
 
-class Disco extends ComponentePC{
-    calcularPrecio(): number {
-        
+class TarjetaGrafica implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 180
     }
 }
 
-class Procesador extends ComponentePC{
-    calcularPrecio(): number {
-        
+class Disco implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 120
     }
 }
 
-let mainClass = () => {
-
+class Procesador implements ComponentePC
+{
+    calcularPrecio(): number
+    {
+        return 450
+    }
 }
+
+let mainComposite = () => {
+    //1. Definir la estructura
+    let comp : Computador = new Computador()
+    let monitor : ComponentePC = new Monitor()
+    let teclado : ComponentePC = new Teclado()
+    let mouse : ComponentePC = new Mouse()
+    let c : Case = new Case()
+    let memoria : ComponentePC = new Memoria()
+    let tg : ComponentePC = new TarjetaGrafica()
+    let disco : ComponentePC = new Disco()
+    let proc : ComponentePC = new Procesador()
+    let placa : ComponentePC = new Placa()
+
+    c.addComponente(memoria)
+    c.addComponente(tg)
+    c.addComponente(disco)
+    c.addComponente(proc)
+    c.addComponente(placa)
+    
+    comp.addComponente(monitor)
+    comp.addComponente(teclado)
+    comp.addComponente(mouse)
+    comp.addComponente(c)
+
+    //2. Ejecutar acción sobre la estructura
+    let precioTotal = comp.calcularPrecio()
+    console.log(`Precio total es : ${precioTotal}`)
+}
+
+mainComposite()
